9ff21f38644eed9cf1f5ad00864032b3f44fcbc7
Franco C Sanchez <francocsanchez@gmail.com>
2022-06-20 21:29:14 -0300

    ejs views

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 require('dotenv').config({ path: 'variables.env' });
+const path = require('path');
 const express = require('express');
 const routers = require('./routers');
 
 const app = express();
 
-app.use('/', routers())
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.use(express.static('public'));
+
+app.use('/', routers());
 
 app.listen(process.env.PORT, () => {
     console.log('Server running... in port ' + process.env.PORT);
