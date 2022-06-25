ac3d5c0c3ccf9a52b268bf112f9ee12e403eaf29
Franco C Sanchez <francocsanchez@gmail.com>
2022-06-25 14:54:42 -0300

    ??

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config({ path: 'variables.env' });
 const path = require('path');
 const express = require('express');
-const routers = require('./routers');
+
+// TODO: Ruteadores
+const routerHome = require('./routers/home.js');
 
 const app = express();
 
@@ -9,8 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static('public'));
-
-app.use('/', routers());
+app.use('/', routerHome);
 
 app.listen(process.env.PORT, () => {
     console.log('Server running... in port ' + process.env.PORT);
