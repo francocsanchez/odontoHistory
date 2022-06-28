4b35ea24a07cd5aa06fce83321d71e110ee1dc11
Franco C Sanchez <francocsanchez@gmail.com>
2022-06-28 14:28:51 -0300

    Config view & file public

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 require('dotenv').config('.env'); // Variables de entorno
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 const dbConnect = require('./config/mongo'); // Archivo de configuracion de BD
 
 const app = express(); // Inicio del servidor
 
+// TODO: Configuracion de servidor
 app.use(cors());
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extends: true }))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'))
+
+app.use(express.static('public'));
 
 // TODO: Main de rutas
 app.use('/users', require('./routes/Users'));
@@ -17,4 +28,5 @@ app.listen(process.env.APP_PORT || 3000, () => {
     console.log('*** Server running ***');
     console.log(`http://localhost/${process.env.APP_PORT || 3000}`);
 })
+
 dbConnect();
